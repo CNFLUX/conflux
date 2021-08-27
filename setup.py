0f550857d1d1ab073dd180f9f056ec9248e8cd13
Xianyi Zhang <zhang39@llnl.gov>
2021-08-27 11:55:12 -0700

    Made the package compilable

--- setup.py
+++ setup.py
@@ -6,20 +6,19 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="example-pkg-YOUR-USERNAME-HERE",
+    name="conflux",
     version="0.0.1",
-    author="Example Author",
-    author_email="author@example.com",
-    description="A small example package",
+    author="Xianyi Zhang",
+    author_email="zhang39@llnl.gov",
+    description="A package to calculate neutrino flux from beta decaying sources",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://lc.llnl.gov/bitbucket/projects/CFX/repos/conflux/browse",
     project_urls={
-        "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
+        "Bug Tracker": "https://lc.llnl.gov/bitbucket/projects/CFX/repos/conflux/browse",
     },
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     package_dir={"": "src"},
