fixed ENSDF Q value incorrect results

conflux/BetaEngine.py
@@ -112,6 +112,7 @@ class BetaBranch(Spectrum):
     # beta spectrum shape as function of energy
     def BetaSpectrum(self, x, nu_spectrum=False):
         Parameters = deepcopy(self.Parameters)
+        
         # prevent out-of-range variable to create insane results
         rangeCorrect = x <= self.E0
 
@@ -163,20 +164,26 @@ class BetaBranch(Spectrum):
             x_high = lower+binwidths
             if x_high > self.E0:
                 x_high = self.E0
-            self.spectrum[k] = (abs(x_high-x_low)
+            thisbinwidth = abs(x_high-x_low)
+            relativewidth = abs(x_high-x_low)/binwidths
+            self.spectrum[k] = (thisbinwidth*relativewidth
                                 *self.BetaSpectrum((x_low+x_high)/2, nu_spectrum))
-            self.uncertainty[k] = (abs(x_high-x_low)
+            if self.ZAI == 521330:
+                print("E0", self.E0, (x_low+x_high)/2, self.spectrum[k])
+            self.uncertainty[k] = (thisbinwidth*relativewidth
                                 *self.SpectUncertMC((x_low+x_high)/2, nu_spectrum))
             if x_high == self.E0:
                 break
-            lower+=binwidths
+            lower += binwidths
         
-        full_range = np.arange(0, 20, 0.01)
+        full_range = np.arange(0, 20, binwidths)
         this_range = np.arange(self.xbins[0], self.xbins[-1], 0.01)
         full_spect = self.BetaSpectrum(full_range, nu_spectrum)
         this_spect = self.BetaSpectrum(this_range, nu_spectrum)
+        if self.ZAI == 521330 and self.E0 == 0.006:
+            print(self.spectrum.sum())
         # normalizing the spectrum
-        norm = self.spectrum.sum()*full_spect.sum()/this_spect.sum()
+        norm = self.spectrum.sum()*full_spect.sum()/this_spect.sum() if self.E0 > binwidths else self.spectrum.sum()
 
         if norm <=0:
             self.spectrum = np.zeros(self.nbin)
@@ -227,6 +234,8 @@ class BetaIstp(Spectrum, Summed):
         Returns:
             None
         """
+        if sigma_E0 > E0:
+            sigma_E0 = E0
         self.branches[E0] = BetaBranch(self.Z, self.A, self.I, self.Q, E0,
                                         sigma_E0, fraction, sigma_frac,
                                         forbiddenness, bAc=bAc, xbins=self.xbins)
@@ -299,7 +308,8 @@ class BetaIstp(Spectrum, Summed):
             di = branchi.sigma_frac
             
             branchi.BinnedSpectrum(nu_spectrum)
-            # branch.spectrum.Norm(branch.spectrum, branch.frac)
+            if self.ZAI == 521330:
+                print(E0i, si, fi)
             self.spectrum += si*fi
             self.spectUnc += branchi.uncertainty*fi
             
@@ -452,9 +462,11 @@ class BetaEngine:
             betaIstp = self.istplist[ZAI]
             if betaIstp.Q < branchErange[0] or betaIstp.Q > branchErange[1]:
                 continue
-
+            
             betaIstp.CalcCovariance(GSF=True)
             betaIstp.SumSpectra(nu_spectrum)
+            if betaIstp.ZAI == 521330:
+                print(betaIstp.spectrum)
             istpCount += 1
 
         endTiming = timeit.default_timer()

conflux/bsg/SpectralFunctions.py
@@ -27,6 +27,8 @@ def fermi_function(W, Z, R, **kwargs):
 
     if Z == 0:
         return f
+    
+    np.seterr(divide='ignore', invalid='ignore', over='ignore')
 
     g = np.sqrt(1-(ALPHA*Z)**2)
     p = np.sqrt(W**2-1)
@@ -88,12 +90,14 @@ def sirlin_g(W, W0, **kwargs):
     g = 0
 
     beta = np.sqrt(W**2-1)/W
-
+    
+    np.seterr(divide='ignore', invalid='ignore')
     g = (3*np.log(PROTON_MASS_W)
         -3./4.
         +4./beta*(-1*spence(1-(2*beta/(1+beta))))
         +4*(np.arctanh(beta)/beta-1)*((W0-W)/(3*W)-3/2+np.log(2*(W0-W)))
         +np.arctanh(beta)/beta*(2*(1+beta**2)+(W0-W)**2/(6*W**2)))
+        
 
     return g
 

examples/BetaExample.py
@@ -8,26 +8,31 @@ import matplotlib.pyplot as plt
 from conflux.BetaEngine import BetaEngine
 
 if __name__ == "__main__":
-    x = np.linspace(0, 10, 200)
+    x = np.linspace(0, 4, 200)
+    binwidth =  1
 
-    testlist = [390960, 390961, 521340, 531340]
+    testlist = [390960, 390961, 521340, 531340, 922390, 932390]
     testEngine = BetaEngine(testlist, xbins=x)
-    testEngine.CalcBetaSpectra(nu_spectrum=True)
+    testEngine.CalcBetaSpectra(nu_spectrum=False)
     
     #print(testEngine.spectralist[521340])
-    index = 531340
+    index = 922390
     fig = plt.figure()
-    plt.errorbar(x, testEngine.istplist[index].spectrum) #, yerr=testEngine.istplist[index].uncertainty)
+    y1 = testEngine.istplist[index].spectrum*binwidth
+    plt.errorbar(x, y1, label=str(index)+"_beta") #, yerr=testEngine.istplist[index].uncertainty)
     # print(max(testEngine.istplist[531340].spectrum), sum(testEngine.istplist[531340].spectrum))
     # testEngine.CalcBetaSpectra(nu_spectrum=False)
     # plt.errorbar(x, testEngine.istplist[531340].spectrum, yerr=testEngine.istplist[531340].uncertainty)
     # print(max(testEngine.istplist[531340].spectrum), sum(testEngine.istplist[531340].spectrum))
 
-    x2 = np.linspace(1, 10, 90)
-    testEngine2 = BetaEngine(testlist, xbins=x2)
-    testEngine2.CalcBetaSpectra(nu_spectrum=True)
     
-    plt.plot(x2, testEngine2.istplist[index].spectrum, ".") #, yerr=testEngine.istplist[index].uncertainty)
+    testEngine2 = BetaEngine(testlist, xbins=x)
+    testEngine2.CalcBetaSpectra(nu_spectrum=True)
+    y2 = testEngine2.istplist[index].spectrum*binwidth
+    print("beta/nu", sum(y1)/sum(y2))
+    plt.xlabel("E (MeV)")
+    plt.plot(x, y2, "--", label=str(index)+"_nu") #, yerr=testEngine.istplist[index].uncertainty)
+    plt.legend()
     plt.show()
     
     # print(testEngine2.istplist[index].branches)

examples/SummationExample.py
@@ -10,7 +10,7 @@ from conflux.FPYEngine import FissionModel, FissionIstp
 from conflux.SumEngine import SumEngine
 
 if __name__ == "__main__":
-    xbins = np.arange(0, 8.25, 0.25)
+    xbins = np.arange(0, 8.25, 0.1)
     
     U235 = FissionIstp(92, 235)
     U235.LoadFissionDB(defaultDB='JEFF')