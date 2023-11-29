fixed the covariance calculation inconsistency

conflux/SumEngine.py
@@ -89,7 +89,6 @@ class SumEngine(Spectrum):
         for FPZAI in fissionModel.FPYlist:
             if FPZAI not in self.FPYlist:
                 self.FPYlist[FPZAI] = fissionModel.FPYlist[FPZAI]
-                print(FPZAI, fissionModel.FPYlist[FPZAI].cov)
                 self.FPYlist[FPZAI].y *= W
                 self.FPYlist[FPZAI].yerr *= W
             else:
@@ -159,6 +158,10 @@ class SumEngine(Spectrum):
                 self.betaSpectraList[FPZAI] = betaSpectraDB.istplist[FPZAI].spectrum
                 self.betaUncertainty[FPZAI] = betaSpectraDB.istplist[FPZAI].uncertainty
                 self.spectrum += self.betaSpectraList[FPZAI]*thisyield
+
+                '''
+                obsolete code
+                '''
                 self.yieldUnc += self.betaSpectraList[FPZAI]*yielderr
                 self.modelUnc += self.betaUncertainty[FPZAI]*thisyield
                 self.spectrumUnc = self.yieldUnc+self.modelUnc
@@ -172,15 +175,21 @@ class SumEngine(Spectrum):
         for i in self.FPYlist:
             for j in self.FPYlist:
                 if i in self.betaSpectraList and j in self.betaSpectraList:
-                    yi = self.FPYlist[i].y
-                    yerri = self.FPYlist[i].yerr
-                    fi = self.betaSpectraList[i]
-                    ferri = self.betaUncertainty[i]
+                    adjustmenti = 1
+                    adjustmentj = 1
+                    if (ifp_begin < ifp_end):
+                        adjustmenti = betaSpectraDB.istplist[i].decay_time_adjust(ifp_begin, ifp_end)
+                        adjustmentj = betaSpectraDB.istplist[j].decay_time_adjust(ifp_begin, ifp_end)
 
-                    yj = self.FPYlist[j].y
-                    yerrj = self.FPYlist[j].yerr
-                    fj = self.betaSpectraList[j]
-                    ferrj = self.betaUncertainty[j]
+                    yi = self.FPYlist[i].y*adjustmenti
+                    yerri = self.FPYlist[i].yerr*adjustmenti
+                    fi = self.betaSpectraList[i]*adjustmenti
+                    ferri = self.betaUncertainty[i]*adjustmenti
+
+                    yj = self.FPYlist[j].y*adjustmentj
+                    # yerrj = self.FPYlist[j].yerr*adjustmentj
+                    fj = self.betaSpectraList[j]*adjustmentj
+                    ferrj = self.betaUncertainty[j]*adjustmentj
 
                     # if covariance matrix were not loaded, make cov diagonal variance
                     cov_ij = 0
@@ -192,8 +201,8 @@ class SumEngine(Spectrum):
                     sigmay_ij = fi*cov_ij*fj
 
                     if (i==j):
-                        self.spectrumUnc += (ferri*yi)**2 + sigmay_ij
+                        self.uncertainty += sigmay_ij # + (ferri*yi)**2
                     else:
-                        self.spectrumUnc += sigmay_ij
+                        self.uncertainty += sigmay_ij
 
-        self.uncertainty = np.sqrt(self.spectrumUnc)
+        self.uncertainty = np.sqrt(self.uncertainty)

examples/testsite.py
@@ -27,7 +27,7 @@ if __name__ == "__main__":
     #U235.CalcCovariance(Ei=0)
 
     model = FissionModel()
-    model.AddContribution(isotope=U235, Ei = 14, fraction=1, IFP=True)
+    model.AddContribution(isotope=U235, Ei = 0.4, fraction=1, IFP=True)
     model.SaveToFile('FPY_235_JEFF_IFP_14MeV.csv')
     # model.AddContribution(isotope=Pu239, Ei = 0.4, fraction=1)
     # model.SaveToFile('FPY_239_JEFF_IFP.csv')
@@ -60,7 +60,7 @@ if __name__ == "__main__":
         ax.set(xlabel='E (MeV)', ylabel='neutrino/MeV', title='U-235 neutrino flux')
         ax.plot(sum_model.xbins, spect, label=str(begin)+' s - '+str(end)+' s')
     ax.legend()
-    fig.savefig("235U_ENDF_jeff_14_MeV_time.png")
+    fig.savefig("235U_ENDF_jeff_0.4_MeV_time.png")
 
     # calcualte cumulative spectrum at time windows after the ignition
     fig, ax = plt.subplots()
@@ -70,15 +70,16 @@ if __name__ == "__main__":
         print('spectrum intergral', sum(spect))
         total_spect+=spect
         ax.set(xlabel='E (MeV)', ylabel='neutrino/MeV', title='U-235 neutrino flux')
-        ax.plot(sum_model.xbins, total_spect, label='by'+str(windows[i+1])+' s')
+        ax.plot(sum_model.xbins, total_spect, label='by'+ str(windows[i+1])+' s')
+        i+=1
     ax.legend()
-    fig.savefig("235U_ENDF_jeff_14_MeV_cumulative.png")
-
+    fig.savefig("235U_ENDF_jeff_0.4_MeV_cumulative.png")
 
     spect_time = np.array(spect_time)
     ybins = windows
     fig, ax = plt.subplots()
-    pcmesh = ax.pcolormesh(xbins, ybins[:-1], spect_time)
+    pcmesh = ax.pcolormesh(xbins, ybins[1:], spect_time)
     ax.set_yscale('log')
+    ax.set_ylim([1e-1, 1e5])
     fig.colorbar(pcmesh, ax=ax)
-    fig.savefig("235U_ENDF_jeff_14_MeV_time_energy.png")
+    fig.savefig("235U_ENDF_jeff_0.4_MeV_time_energy.png")