2e1bbb930d27abf97bc4e0eba7ef74b6d025ae6a
AnoshIrani <96199927+AnoshIrani@users.noreply.github.com>
2024-08-01 15:03:59 -0500

    Switched Default Beta Database
    Line 358 >> Tried loading ENSDFbetaDB2.xml, but this file is not part of the data package we include in CONFLUX. reverted back to ENSDFbetaDB.xml

--- conflux/BetaEngine.py
+++ conflux/BetaEngine.py
@@ -554,7 +554,7 @@ class BetaEngine:
         
         self.custom_func=custom_func
 
-    def LoadBetaDB(self, targetDB=CONFLUX_DB+"/betaDB/ENSDFbetaDB2.xml"):
+    def LoadBetaDB(self, targetDB=CONFLUX_DB+"/betaDB/ENSDFbetaDB.xml"):
         """Load default or input betaDB to obtain beta decay informtion
         """
         useInputList = True # test if the engine is defined with an inputlist
