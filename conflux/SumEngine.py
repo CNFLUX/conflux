solved the bug with editbranch and updated uncertainty calculation

conflux/SumEngine.py
@@ -59,6 +59,8 @@ class SumEngine(Spectrum):
         self.FPYlist = {}
         self.betaSpectraList = {}
         self.betaUncertainty = {}
+        self.countlist = {}
+        self.d_countlist = {}
         self.nu_spectrum = nu_spectrum
 
         self.betaDB = betaSpectraDB
@@ -100,7 +102,7 @@ class SumEngine(Spectrum):
             Returns:
                 None
         """
-        assert isotope.xbins == self.xbins, "binning of two spectra are different"
+        assert len(isotope.xbins) == self.nbin, "binning of two spectra are different"
 
         self.betaSpectraList[istpname] = isotope.spectrum
         self.betaUncertainty[istpname] = isotope.uncertainty
@@ -134,11 +136,12 @@ class SumEngine(Spectrum):
 
     def AddBetaIstp(self, betaIstp, istpname, count=1, d_count=0):
         """
-            NEW
+            NEW TODO
             Add contribution of individual beta decaying isotope into summation
             model
         """
-        self.betaDB.istplist[ZAI] = betaIstp
+        thisZAI = betaIstp.ZAI
+        self.betaDB.istplist[thisZAI] = betaIstp
 
         self.betaSpectraList[istpname] = betaIstp.spectrum
         self.betaUncertainty[istpname] = betaIstp.uncertainty
@@ -148,7 +151,7 @@ class SumEngine(Spectrum):
         # self.spectrum += self.betaDB.istplist[ZAI].spectrum*count
         # self.uncertainty += self.betaDB.istplist[ZAI].spectrum*count
 
-    def EditIstp(self, istpname, count, d_count):
+    def EditContribution(self, istpname, count, d_count):
         """
             NEW TODO
         """
@@ -169,9 +172,19 @@ class SumEngine(Spectrum):
         self.modelUnc = np.zeros(self.nbin)
         self.yieldUnc = np.zeros(self.nbin)
 
-        for ZAI in self.betaSpectraList.keys():
-            self.spectrum += self.betaSpectraList[ZAI]*count
-            self.uncertainty += self.betaUncertainty[ZAI]*count
+        for istp in self.betaSpectraList.keys():
+            count = self.countlist[istp]
+            d_count = self.d_countlist[istp]
+
+            relunc_a = self.betaUncertainty[istp]/self.betaSpectraList[istp]
+            relunc_b = d_count/count
+            this_spec = self.betaSpectraList[istp]*count
+            this_unc2 = (this_spec)**2*(relunc_a**2+relunc_b**2)
+
+            self.spectrum += this_spec
+            self.uncertainty += this_unc2
+
+        self.uncertainty = np.sqrt(self.uncertainty)
 
     # method to add fission/non-fissile/non-equilibrium isotopes into the engine
     def AddModel(self, fissionModel, W=1.0):

examples/NewSeqExample.py
@@ -5,17 +5,22 @@ import csv
 import operator
 
 # conflux modules
-from conflux.BetaEngine import BetaEngine
+from conflux.BetaEngine import BetaEngine, BetaIstp
 from conflux.FPYEngine import FissionModel, FissionIstp
 from conflux.SumEngine import SumEngine
 
 if __name__ == "__main__":
     # Define spectrum binning
-    xbins = np.arange(0, 20, 0.1)
+    xbins = np.arange(0, 10, 0.1)
 
     # Calculate individual beta spect
     betaSpectraDB = BetaEngine(xbins=xbins)
     betaSpectraDB.CalcBetaSpectra(nu_spectrum=True, branchErange=[0.0, 20.0])
+
+    Pu239 = FissionIstp(94, 239, Ei = 0)
+    Pu239.LoadFissionDB()
+    Pu239.LoadCorrelation()
+    Pu239.CalcBetaSpectra(betaSpectraDB)
 
     # Define and calculate fission and nu spect
     U235 = FissionIstp(92, 235, Ei = 0.5, DB='ENDF', IFPY=False)
@@ -29,23 +34,20 @@ if __name__ == "__main__":
     U235T.LoadCorrelation(DB='ENDF')
     U235T.CalcBetaSpectra(betaSpectraDB)
 
-    Pu239 = FissionIstp(94, 239, Ei = 0)
-    Pu239.LoadFissionDB()
-    Pu239.LoadCorrelation()
-    Pu239.CalcBetaSpectra(betaSpectraDB)
+
 
     # Define summation engine
     newsum = SumEngine(betaSpectraDB)
 
     # Add fission spect
-    newsum.AddFissionIstp(U235, "U235", 100, 10)
-    newsum.AddFissionIstp(Pu239, "Pu239", 50, 7)
+    newsum.AddFissionIstp(U235, "U235", 100, 0)
+    # newsum.AddFissionIstp(Pu239, "Pu239", 50, 7)
 
     pu241beta = BetaIstp(94, 241, 0, 21.5, 200, "Pu241beta", xbins=xbins)
 
     # Add beta spect
-    newsum.AddBetaIstp(betaSpectraDB.istplist[922390], "U239", count = 750, d_count = 2)
-    newsum.AddBetaIstp(pu241beta, "Pu241beta", count = 40, d_count = 2)
+    # newsum.AddBetaIstp(betaSpectraDB.istplist[922390], "U239", count = 750, d_count = 2)
+    # newsum.AddBetaIstp(pu241beta, "Pu241beta", count = 40, d_count = 2)
 
     # sum all spect
     newsum.CalcReactorSpectrum()
@@ -56,14 +58,14 @@ if __name__ == "__main__":
     # # edit contributions freely
     # for i in range(50):
     #     u239number = 750-2*i
-    #     newsum.EditIstp("U239", count = u239number, d_count = 2)
-    #     newsum.EditIstp("Pu241beta", count = 40-i, d_count = 2)
+    #     newsum.EditContribution("U239", count = u239number, d_count = 2)
+    #     newsum.EditContribution("Pu241beta", count = 40-i, d_count = 2)
     #     # recalc spect each time
     #     newsum.CalcReactorSpectrum()
 
-    # fig, ax = plt.subplots()
-    # ax.set_xlim([0, 10])
-    # ax.set(xlabel='E (MeV)', ylabel='neutrino count')
-    # ax.errorbar(newsum.xbins, newsum.spectrum, yerr=newsum.uncertainty, label="test spectrum")
-    # ax.legend()
-    # plt.show()
+    fig, ax = plt.subplots()
+    ax.set_xlim([0, 10])
+    ax.set(xlabel='E (MeV)', ylabel='neutrino count')
+    ax.errorbar(newsum.xbins, newsum.spectrum, yerr=newsum.uncertainty, label="test spectrum")
+    ax.legend()
+    plt.show()