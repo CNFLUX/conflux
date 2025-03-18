Fix IBD cross section units note (cm^2, not m^2)

conflux/IBDXSection.py
@@ -2,23 +2,28 @@
 
 import numpy as np
 
-def ibd_xsection(enu):
-    """An empirical IBD cross section function
-    Phys. Rev. D 60 053003 (Vogel model)
+def ibd_xsection_cm2(Enu):
+    """
+    IBD interaction cross section in cm^2 / proton.
 
-    :param enu: electron anitneutrino energies array, in MeV
-    :return: inverse beta decay cross section in m^2
-    :rtype: float"""
+    From: P. Vogel, J. F. Beacom:
+    "An empirical IBD cross section function"
+    Phys. Rev. D 60 053003
+    Equation 9
 
-    epos = enu - 1.29   # positron energy, MeV
-    epos[epos < 0] = 0
-    ppos = np.sqrt(epos**2-0.511**2)
+    :param Enu: electron anitneutrino energies array, in MeV
+    :return: inverse beta decay cross section in cm^2
+    :rtype: float
+    """
 
-    # tau_n = 877.75
-    # fr = 1.7152
-    # xsection = 2*np.pi/0.511**5/(fr*tau_n)*epos*ppos
+    # positron total energy, MeV $E_e^{(0)} = E_\nu - (m_n - m_p)$
+    Ee0 = Enu - 1.29333236
+    Ee0[Ee0 <= 0.511] = 0.511  # truncate to pe0 = 0
 
-    xsection = 0.0952 * epos * ppos * 1e-42
+    # positron momentum
+    pe0 = np.sqrt(Ee0**2 - 0.511**2)
+
+    xsection = 0.0952 * Ee0 * pe0 * 1e-42
     xsection[np.isnan(xsection)] = 0
 
     return xsection

examples/final_examples/fission_burst.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 # conflux modules
 from conflux.BetaEngine import BetaEngine
 from conflux.FPYEngine import FissionIstp
-from conflux.IBDXSection import ibd_xsection
+from conflux.IBDXSection import ibd_xsection_cm2
 
 E_fission = 3.2e-11                 # energy per fission, joules
 E_TNT = 4.184e9                     # energy per ton TNT, joules
@@ -32,7 +32,7 @@ if __name__ == "__main__":
     U235.CalcBetaSpectra(betaSpectraDB)
     totalspect = U235.spectrum * test_flux # the full spectrum summed with all fission products
 
-    totalspect_ibd = totalspect*ibd_xsection(xbins) # the IBD detector spectrum
+    totalspect_ibd = totalspect*ibd_xsection_cm2(xbins) # the IBD detector spectrum
 
     # define the time windows of the calculation
     windows = np.linspace(0, 100, 101)
@@ -60,7 +60,7 @@ if __name__ == "__main__":
         spect_time.append(spect)
         spect_sum_t.append(100 * sum(cumuspect) / sum(totalspect))
         spect_sum_t_thresh.append(100 * sum(cumuspect[xbins > 1.8]) / sum(totalspect[xbins > 1.8]))
-        spect_sum_t_ibd.append(100 * sum(cumuspect*ibd_xsection(xbins)) / sum(totalspect_ibd))
+        spect_sum_t_ibd.append(100 * sum(cumuspect*ibd_xsection_cm2(xbins)) / sum(totalspect_ibd))
     
         ax.plot(xbins, cumuspect, label="%g s - %g s"%(begin, end))
         