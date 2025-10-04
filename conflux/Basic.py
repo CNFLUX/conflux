13dea8d4b3b96203213c4ba844f4559454b79808
xzhan135 <xianyizhang39@gmail.com>
2025-10-03 18:15:10 -0700

    initial neutrino engine commit

--- conflux/Basic.py
+++ conflux/Basic.py
@@ -18,7 +18,7 @@ class Spectrum:
         """Contents of the spectrum"""
         self.uncertainty = np.zeros(self.nbin)
         """Spectrum content uncertainties"""
-        self.covariance = np.zeros(self.nbin)
+        self.cov_matrix = np.zeros(self.nbin, self.nbin)
         """Spectrum content covariance"""
         self.integral = 0
         """Integral of spectrum"""
