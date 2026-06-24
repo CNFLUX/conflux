fixed the NaN issue of covariance matrix in conversion mode

conflux/NeutrinoEngine.py
@@ -7,6 +7,7 @@
 
 """Public modules"""
 import numpy as np
+import pandas as pd
 import os
 import matplotlib.pyplot as plt
 from copy import deepcopy
@@ -49,8 +50,27 @@ class NeutrinoData(Spectrum):
     def __init__(self, inputDB, covarianceDB):
         self.inputDB = inputDB
         self.covarianceDB=covarianceDB
+        
         self.LoadNeutrinoDB(self.inputDB, self.covarianceDB)
 
-    def LoadNeutrinoDB(self, inputDB, covarianceDB):
-        with open(os.path.expandvars(inputDB), newline='') as inputCSV:
-            inputreader = csv.DictReader(inputCSV, delimiter=',', quotechar='|')
+    def LoadNeutrinoDB(self, inputDB, covarianceDB):        
+        df = pd.read_csv(inputDB,  comment='#')
+        self.x = df["E"].to_numpy()
+        self.y = df["Content"].to_numpy()
+
+        Spectrum.__init__(self, self.x)
+
+        self.spectrum = self.y
+        self.cov_matrix = np.loadtxt(covarianceDB, delimiter=',')
+        self.uncertainty = np.sqrt(self.cov_matrix.diagonal())
+
+if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+    testnudata = []
+    testnudata.append(NeutrinoData(f"{CONFLUX_DB}/neutrinoDB/U235_FLUX_DYB.csv", f"{CONFLUX_DB}/neutrinoDB/U235_FLUX_COV_DYB_ABS.csv"))
+    testnudata.append(NeutrinoData(f"{CONFLUX_DB}/neutrinoDB/U235_FLUX_STEREO.csv", f"{CONFLUX_DB}/neutrinoDB/U235_FLUX_COV_STEREO.csv"))
+
+    for i in range(len(testnudata)):
+        plt.errorbar(testnudata[i].xbins, testnudata[i].spectrum, testnudata[i].uncertainty)
+    # plt.ylim([0, 5e-43])
+    plt.show()